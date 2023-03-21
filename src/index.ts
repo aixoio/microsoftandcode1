8c368e844fdf3ada700e7be9cd2b154f84c705cd
aixoio <79969530+aixoio@users.noreply.github.com>
2023-03-21 17:15:25 +1100

    Setup dist

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import $ from "jquery"
-
-import "./index.scss"
-
-fetch(" https://dog.ceo/api/breeds/image/random").then(d => d.json()).then(data => {
-    
-    $("#dogimg").attr("src", data.message)
-
-})
