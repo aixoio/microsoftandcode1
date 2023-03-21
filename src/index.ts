363cbe7a770885b39fe27ffbb91a96d35c472ad9
aixoio <79969530+aixoio@users.noreply.github.com>
2023-03-21 17:10:04 +1100

    Add image

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import $ from "jquery"
+
 import "./index.scss"
+
+fetch(" https://dog.ceo/api/breeds/image/random").then(d => d.json()).then(data => {
+    
+    $("#dogimg").attr("src", data.message)
+
+})
